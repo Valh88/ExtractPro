2bceaab231d78503f3f67cf4144fb61a38b7af90
Vano <simplmd@gmail.com>
2026-07-17 18:36:13 +0700

    up

--- Extract/code/views/lobby_view/gameviewmarket.pas
+++ Extract/code/views/lobby_view/gameviewmarket.pas
@@ -2,10 +2,17 @@ unit GameViewMarket;
 
 interface
 
-uses Classes,
-  CastleVectors, CastleUIControls, CastleControls, CastleKeysMouse;
+uses Classes, SysUtils,
+  CastleVectors, CastleUIControls, CastleControls, CastleKeysMouse, CastleColors;
 
 type
+  TMarketItemData = record
+    Name: string;
+    ItemType: string;
+    Description: string;
+    Price: Integer;
+  end;
+
   TViewMarket = class(TCastleView)
   published
     MarketDesign: TCastleDesign;
@@ -19,6 +26,8 @@ type
     constructor Create(AOwner: TComponent); override;
     procedure Start; override;
     procedure Stop; override;
+    procedure AddItemRow(const AData: TMarketItemData);
+    procedure ClearItems;
   end;
 
 var
@@ -47,6 +56,78 @@ end;
 procedure TViewMarket.Stop;
 begin
   inherited;
+end;
+
+procedure TViewMarket.AddItemRow(const AData: TMarketItemData);
+var
+  Row: TCastleHorizontalGroup;
+  Slot: TCastleImageControl;
+  NameLbl, TypeLbl, DescLbl, PriceLbl: TCastleLabel;
+  Idx: Integer;
+  Prefix: string;
+begin
+  Idx := ItemsList.ControlsCount;
+  Prefix := Format('ItemRow%d_', [Idx]);
+
+  Row := TCastleHorizontalGroup.Create(Self);
+  Row.Height := 56;
+  Row.Spacing := 6;
+  Row.WidthFraction := 1.0;
+
+  Slot := TCastleImageControl.Create(Self);
+  Slot.Name := Prefix + 'Slot';
+  Slot.Url := 'castle-data:/ui/slot_bg.png';
+  Slot.Width := 56;
+  Slot.Height := 56;
+  Row.InsertFront(Slot);
+
+  NameLbl := TCastleLabel.Create(Self);
+  NameLbl.Name := Prefix + 'Name';
+  NameLbl.Caption := AData.Name;
+  NameLbl.AutoSize := False;
+  NameLbl.Width := 160;
+  NameLbl.Height := 56;
+  NameLbl.VerticalAlignment := vpMiddle;
+  NameLbl.Color := Vector4(0.85, 0.85, 0.85, 1.0);
+  Row.InsertFront(NameLbl);
+
+  TypeLbl := TCastleLabel.Create(Self);
+  TypeLbl.Name := Prefix + 'Type';
+  TypeLbl.Caption := AData.ItemType;
+  TypeLbl.AutoSize := False;
+  TypeLbl.Width := 80;
+  TypeLbl.Height := 56;
+  TypeLbl.VerticalAlignment := vpMiddle;
+  TypeLbl.Color := Vector4(0.6, 0.6, 0.6, 1.0);
+  Row.InsertFront(TypeLbl);
+
+  DescLbl := TCastleLabel.Create(Self);
+  DescLbl.Name := Prefix + 'Desc';
+  DescLbl.Caption := AData.Description;
+  DescLbl.AutoSize := False;
+  DescLbl.Width := 200;
+  DescLbl.Height := 56;
+  DescLbl.VerticalAlignment := vpMiddle;
+  DescLbl.Color := Vector4(0.7, 0.7, 0.7, 1.0);
+  Row.InsertFront(DescLbl);
+
+  PriceLbl := TCastleLabel.Create(Self);
+  PriceLbl.Name := Prefix + 'Price';
+  PriceLbl.Caption := Format('%dg', [AData.Price]);
+  PriceLbl.AutoSize := False;
+  PriceLbl.Width := 80;
+  PriceLbl.Height := 56;
+  PriceLbl.VerticalAlignment := vpMiddle;
+  PriceLbl.Color := Vector4(1.0, 0.85, 0.3, 1.0);
+  Row.InsertFront(PriceLbl);
+
+  ItemsList.InsertFront(Row);
+end;
+
+procedure TViewMarket.ClearItems;
+begin
+  while ItemsList.ControlsCount > 0 do
+    ItemsList.RemoveControl(ItemsList.Controls[0]);
 end;
 
 end.
